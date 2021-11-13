3a7fe1e8a128dd7a3e65781aabcc8941aaaa9760
Clemy-beep <clementine.digny@gmail.com>
2021-11-13 23:45:44 +0100

    username check before register

--- model/register.php
+++ model/register.php
@@ -5,16 +5,21 @@ $username = isset($_POST["username"]) ? $_POST['username'] : null;
 $password = isset($_POST["password"]) ? $_POST['password'] : null;
 $email = isset($_POST["email"]) ? $_POST['email'] : null;
 
-try{
-    $stmt = $pdo->prepare("INSERT INTO `users` (`username`, `password` , `email`) VALUES (:username, :password, :email)");
-    $stmt->execute(['username' => $username , 'password' => $password, 'email' => $email,]);
-    include('../vues/success.html');
-}catch(err){
-    include('../vues/error/html');
-    print(err);
+$stmt = $pdo->prepare("SELECT username FROM users WHERE username = :username");
+$stmt->execute(['username' => $username]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (isset($user) && !empty($user)) {
+    include('../vues/userexists.html');
+} 
+else {
+    try{
+        $stmt = $pdo->prepare("INSERT INTO `users` (`username`, `password` , `email`) VALUES (:username, :password, :email)");
+        $stmt->execute(['username' => $username , 'password' => $password, 'email' => $email,]);
+        include('../vues/success.html');
+    }catch(err){
+        include('../vues/error.html');
+        print(err);
+    }
 }
-
-
-
-
 ?>
