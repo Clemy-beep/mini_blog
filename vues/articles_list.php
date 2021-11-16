70ed562c8d3d7db0507dc7226b2ce68b720fb539
Clemy-beep <clementine.digny@gmail.com>
2021-11-16 11:17:59 +0100

    prevent css to be loaded in cache

--- vues/articles_list.php
+++ vues/articles_list.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All articles</title>
-    <link rel="stylesheet" href="../articles_style.css">
+    <link rel="stylesheet" href="../articles_style.css?<?php echo time(); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@1,500&display=swap" rel="stylesheet">
