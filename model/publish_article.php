ab2027539f0152d2637985f788566f1114904b4c
Clemy-beep <clementine.digny@gmail.com>
2021-11-18 16:54:58 +0100

    new front

--- model/publish_article.php
+++ model/publish_article.php
@@ -6,9 +6,14 @@ $title = isset($_POST["title"]) ? $_POST['title'] : null;
 $content = isset($_POST["content"]) ? $_POST['content'] : null;
 
     try{
-        $stmt = $pdo->prepare("INSERT INTO `articles` (`title`, `content` , `published_on, `author`) VALUES (:title, :content,NOW(), :author)");
+        if(isset($title) && isset($content)){
+        $stmt = $pdo->prepare("INSERT INTO `articles`(`title`, `content`, `published_on`, `author`) VALUES (:title, :content,NOW(), :author)");
         $stmt->execute(['title' => $title , 'content' => $content, 'author' =>  $loggeduser,]);
-        include('../vues/article_added.html');
+        echo "success";
+        }
+        else{
+            echo"<span>Plese fill all the fields before submitting your article.</span>";
+        }
     }
     catch(Exception $err){
         echo $err;
--- vues/articles_list.php
+++ vues/articles_list.php
@@ -3,6 +3,9 @@
 
 <?php 
     require_once '../model/log_infos.php';
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
 ?>
 
 <head>
